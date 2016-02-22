67cbaf3493f5eacdb281e40b552b12f9d2b6f30e
Tim Berry <timothyaberry@gmail.com>
2016-02-22 11:55:38 -0700

    Initial routing setup

---
+++ www/app/app.routes.js
@@ -0,0 +1,96 @@
+(function() {
+'use strict';
+
+angular.module('waitrApp')
+  .config(configure)
+
+  configure.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function configure($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      // LOGIN - REGISTER ROUTES
+      // .state('login', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('customer-sign-up', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('restaurant-sign-up', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+
+
+      // CUSTOMER ROUTES
+      // .state('customer', {
+      //   url: '/customer',
+      //   abstract: true,
+      //   templateUrl: '',
+      //   controller: ''
+      // })
+      // .state('customer.home', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('customer.restaurant', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('customer.restaurant.menu', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('customer.settings', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('customer.waitlist', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+
+
+      // RESTAURANT ROUTES
+      // .state('restaurant', {
+      //   url: '/restaurant',
+      //   abstract: true,
+      //   templateUrl: '',
+      //   controller: ''
+      // })
+      // .state('restaurant.home', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('restaurant.queue', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('restaurant.profile', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      // .state('restaurant.menu', {
+      //   url: '',
+      //   templateUrl: '',
+      //   controller:''
+      // })
+      ;
+    }
+
+})();
