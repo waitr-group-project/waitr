4ce0ecee1ab30b586ab70297558ee4149dc375b7
Dan Le <dhl1337@gmail.com>
2016-02-22 23:25:32 -0700

    saving point

--- www/app/app.routes.js
+++ www/app/app.routes.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('waitrApp')
-  .config(configure)
+  .config(configure);
 
   configure.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -52,37 +52,42 @@ angular.module('waitrApp')
       //   templateUrl: '',
       //   controller:''
       // })
-      // .state('customer.settings', {
-      //   url: '',
-      //   templateUrl: '',
-      //   controller:''
-      // })
-      // .state('customer.waitlist', {
-      //   url: '',
-      //   templateUrl: '',
-      //   controller:''
-      // })
+       .state('customer.setting', {
+         url: '/setting',
+         templateUrl: 'app/customer/setting/setting.html'
+         //controller:''
+       })
+      .state('customer.edit', {
+        url: '/setting/edit',
+        templateUrl: 'app/customer/editContact/editContact.html'
+        //controller:''
+      })
+       .state('customer.waitlist', {
+         url: '/waitlist',
+         templateUrl: 'app/customer/waitList/waitList.html'
+         //controller:''
+       })
 
 
       // RESTAURANT ROUTES
-      .state('restaurant', {
-        url: '/restaurant',
-        abstract: true,
-        templateUrl: 'app/restaurant/restaurant.html',
-        controller: 'restaurantCtrl',
-        controllerAs: 'rc'
-      })
+      //.state('restaurant', {
+      //  url: '/restaurant',
+      //  abstract: true,
+      //  templateUrl: 'app/restaurant/restaurant.html',
+      //  controller: 'restaurantCtrl',
+      //  controllerAs: 'rc'
+      //})
       // .state('restaurant.home', {
       //   url: '',
       //   templateUrl: '',
       //   controller:''
       // })
-      .state('restaurant.queue', {
-        url: '/queue',
-        templateUrl: 'app/restaurant/resQueue/resQueueT.html',
-        controller:'resQueueCtrl',
-        controllerAs: 'rqc'
-      })
+      //.state('restaurant.queue', {
+      //  url: '/queue',
+      //  templateUrl: 'app/restaurant/resQueue/resQueueT.html',
+      //  controller:'resQueueCtrl',
+      //  controllerAs: 'rqc'
+      //})
       // .state('restaurant.profile', {
       //   url: '',
       //   templateUrl: '',
