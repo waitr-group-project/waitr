ed09870075f06dcbd7774cebff6506ee443bdea9
Cleber Albuquerque <cleber.lop@gmail.com>
2016-02-24 10:20:13 -0700

    stoped at restaProfile

--- www/app/app.routes.js
+++ www/app/app.routes.js
@@ -8,7 +8,7 @@ angular.module('waitrApp')
 
   function configure($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/customer/restaurantsList');
+    $urlRouterProvider.otherwise('/customer/home');
 
     $stateProvider
       // LOGIN - REGISTER ROUTES
@@ -145,7 +145,7 @@ angular.module('waitrApp')
         url: '/profile/menu',
         templateUrl: './app/restaurant/restaProfile/restaMenu.html',
         controller: 'restaProfileCtrl',
-        controllerAs: 'rmc'
+        controllerAs: 'rpc'
       })
 
       .state('restaurant.settings', {
