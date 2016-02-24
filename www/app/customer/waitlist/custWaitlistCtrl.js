a28d21b3e48ae619fb440a0a9f3a34229c14f61e
Dan Le <dhl1337@gmail.com>
2016-02-24 09:27:58 -0700

    pulling recent update on model

--- www/app/customer/waitlist/custWaitlistCtrl.js
+++ www/app/customer/waitlist/custWaitlistCtrl.js
@@ -5,6 +5,17 @@
 
   function custWaitlistCtrl () {
     var cwc = this;
+
+    var data = {
+      img: '',
+      address: '',
+      waitTime: '',
+      type: '',
+      parties: 02,
+      timeRaining: 10
+    }
+
+
   }
 
 })();
