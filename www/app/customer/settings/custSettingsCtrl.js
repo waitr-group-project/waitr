888d7c92e5e41ad55c9b7d3199a2af959abfbbe5
Dan Le <dhl1337@gmail.com>
2016-02-23 21:42:20 -0700

    working on settings

--- www/app/customer/settings/custSettingsCtrl.js
+++ www/app/customer/settings/custSettingsCtrl.js
@@ -3,13 +3,15 @@
     .module('waitrApp')
     .controller('custSettingsCtrl', [custSettingsCtrl]);
 
-  function custSettingsCtrl () {
+  function custSettingsCtrl ($cordovaVibration) {
     var csc = this;
 
+    //$cordovaVibration.vibrate(500);
 
+    //csc.vibrate = false;
+    csc.vibrate = {checked: true};
 
-
-
+    console.log(csc.vibrate);
   }
 
 })();
