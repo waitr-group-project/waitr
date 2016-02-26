ab7d66a60b9e5a9b8c6513db142d67bdd51370d4
Cleber Albuquerque <cleber.lop@gmail.com>
2016-02-26 16:26:04 -0700

    created hours objs in restaSettingsCtrl

--- www/app/restaurant/restaSettings/restaSettingsCtrl.js
+++ www/app/restaurant/restaSettings/restaSettingsCtrl.js
@@ -6,7 +6,16 @@
   function restaSettingsCtrl () {
     var rsc = this;
 
-  
+    rsc.hours = {
+      monday: { openTime: rsc.mondayOpen, closeTime: rsc.mondayClose },
+      tuesday: { openTime: rsc.tuesdayOpen, closeTime: rsc.tuesdayClose },
+      wednesday: { openTime: rsc.wednesdayOpen, closeTime: rsc.wednesdayClose },
+      thursday: { openTime: rsc.thursdayOpen, closeTime: rsc.thursdayClose },
+      friday: { openTime: rsc.fridayOpen, closeTime: rsc.fridayClose},
+      saturday: { openTime: rsc.saturdayOpen, closeTime: rsc.saturdayClose},
+      sunday: { openTime: rsc.sundayOpen, closeTime: rsc.sundayClose}
+    };
+
 
   }
 
