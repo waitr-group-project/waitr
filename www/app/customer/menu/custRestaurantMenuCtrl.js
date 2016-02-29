acd16c5659527a06db9345a79d105d97124c348d
Samuel Roberts <srob11221@gmail.com>
2016-02-29 10:37:58 -0700

    finished user restaurant menu

--- www/app/customer/menu/custRestaurantMenuCtrl.js
+++ www/app/customer/menu/custRestaurantMenuCtrl.js
@@ -1,36 +1,44 @@
 (function () {
   angular
     .module('waitrApp')
-    .controller('custRestaurantMenuCtrl', ['restaurantService', 'userService', 'waitlistService','$stateParams', '$ionicHistory', '$state', custRestaurantMenuCtrl]);
+    .controller('custRestaurantMenuCtrl', ['restaurantService', 'userService', 'waitlistService','$stateParams', '$ionicHistory', '$state', '$ionicSideMenuDelegate', custRestaurantMenuCtrl]);
 
-  function custRestaurantMenuCtrl (restaurantService, userService, waitlistService, $stateParams, $ionicHistory, $state) {
-
+  function custRestaurantMenuCtrl (restaurantService, userService, waitlistService, $stateParams, $ionicHistory, $ionicSideMenuDelegate, $state) {
     var cmc = this;
-    cmc.menuSection = '';
-
-    cmc.restaurantId = "56cf854d507ee272a9dc2dbb"
-
+    // cmc.menuSection = [];
+    cmc.restaurantId = "56cf854d507ee272a9dc2dbb";
+    cmc.menuTitle= 'PICK A SECTION';
+    cmc.menuContent = cmc.groupedMenu;
     restaurantService.getRestaurantMenu(cmc.restaurantId).then(function (restaurant) {
-      console.log('restaurant',restaurant);
-      cmc.restaurant = restaurant;
-      cmc.restaurantMenu = restaurant.menu;
-      console.log('menu', restaurant.menu);
+        console.log('restaurant',restaurant);
+        cmc.restaurant = restaurant;
+        // cmc.restaurantMenu = restaurant.menu;
+        console.log('menu', restaurant.menu);
+        cmc.groupedMenu = _.groupBy(restaurant.menu, 'section');
+        return console.log(cmc.groupedMenu);
     });
     cmc.goBack = function() {
        $ionicHistory.goBack();
     };
-
+    cmc.toggleSection = function(key, value) {
+        cmc.menuTitle = key;
+        cmc.menuContent = value;
+        return console.log(key, value)
+    }
+    
     
     //     $state.go("restaurant.home");
+    //   })
+    // };
 
-
-
+    // cmc.SectionToggle = true;
 
     // cmc.showSectionOnClick = function(value) {
     //   cmc.SectionToggle = value;
     // };
 
-
   }
 
 })();
+
+// <button ng-click="cmc.toggleRight()" showBar(cmc.show) style="color:white" class="button button-icon icon ion-navicon"></button>
