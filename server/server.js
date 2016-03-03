b2c077b6c77a50478776e548bc8eb5c7ac3b1f26
Turner Houghton <Turner Houghton>
2016-03-03 15:02:19 -0700

    committing reverted server.js file

--- server/server.js
+++ server/server.js
@@ -95,5 +95,5 @@ io.on('connection', function(socket) {
 });
 
 http.listen(port, function() {
-  console.log("listening on " + config.getIp() + ":" + port);
+  console.log("listening on port" + port);
 });
