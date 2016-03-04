6598ac3b00321b799541f7b1bd4dfeb974819754
Tim Berry <timothyaberry@gmail.com>
2016-03-04 08:56:05 -0700

    changed app_id and added fonts folder

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3439,7 +3439,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"68c0a725","api_key":"d674087ef12e5144c3a247333dc8c46f53c8b97292bc503d"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"7812d2dc","api_key":"d674087ef12e5144c3a247333dc8c46f53c8b97292bc503d"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 
 }).finish();
 
